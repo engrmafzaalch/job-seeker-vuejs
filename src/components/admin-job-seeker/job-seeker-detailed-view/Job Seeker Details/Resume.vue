eede72a955ded82bf187f14e44a375b04172494f
hassam <malik.hassam531@gmail.com>
2021-03-29 15:55:07 +0500

    admin page update

--- src/components/admin-job-seeker/job-seeker-detailed-view/Job Seeker Details/Resume.vue
+++ src/components/admin-job-seeker/job-seeker-detailed-view/Job Seeker Details/Resume.vue
@@ -4,8 +4,10 @@
   <div class="row">
     <div class="col-12 card">
       <div class="card-body">
+<!--        loop can be used at here-->
+        <div >
         <div class="row">
-          <div class="col-12">
+          <div class="col-12" >
             <h3 class="text-primary head_">CV</h3>
           </div>
         </div>
@@ -22,7 +24,7 @@
                   <span>CV.pdf</span>
                 </div>
                 <div>
-                  <span class="text-black-50">182 KB</span>
+                  <span class="text-black-50" >{{resume.certificates}}</span>
                 </div>
               </div>
               <div class="col-2"  @click="deleteresume()">
@@ -38,80 +40,81 @@
             </div>
           </div>
         </div>
-
-        <hr class="py-2">
-
-        <div class="row">
-          <div class="col-12">
-            <h3 class="text-primary head_">Degree Certificate</h3>
-          </div>
-        </div>
-        <div class="row pb-2">
-          <div class="col-lg-4 border border-light rounded">
-            <div class="row pt-2">
-              <div class="col-1">
-                <a href="#">
-                  <img class="mt-1" src="./Group.png" alt="Group" height="24" width="auto">
-                </a>
-              </div>
-              <div class="col-6">
-                <div>
-                  <span>Degree.pdf</span>
-                </div>
-                <div>
-                  <span class="text-black-50">182 KB</span>
-                </div>
-              </div>
-              <div class="col-2"  @click="deleteresume()">
-                <a href="#">
-                  <img class="float-right mt-1" src="./trash.png" height="20" width="auto">
-                </a>
-              </div>
-              <div class="col-2" @click=" downloadresume()">
-                <a href="#">
-                  <img class="mr-2 mt-1" src="./download.png" height="20" width="auto">
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
 
-        <hr class="py-2">
+<!--        <hr class="py-2">-->
 
-        <div class="row">
-          <div class="col-12">
-            <h3 class="text-primary head_">NYSC Discharge Certificate</h3>
-          </div>
-        </div>
-        <div class="row pb-2">
-          <div class="col-lg-4 border border-light rounded">
-            <div class="row pt-2">
-              <div class="col-1">
-                <a href="#">
-                  <img class="mt-1" src="./Group.png" alt="Group" height="24" width="auto">
-                </a>
-              </div>
-              <div class="col-6">
-                <div>
-                  <span>NYSC.pdf</span>
-                </div>
-                <div>
-                  <span class="text-black-50">182 KB</span>
-                </div>
-              </div>
-              <div class="col-2" @click="deleteresume()">
-                <a href="#">
-                  <img class="float-right mt-1" src="./trash.png" height="20" width="auto">
-                </a>
-              </div>
-              <div class="col-2" @click=" downloadresume()">
-                <a >
-                  <img class="mr-2 mt-1" src="./download.png" height="20" width="auto">
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+<!--        <div class="row">-->
+<!--          <div class="col-12">-->
+<!--            <h3 class="text-primary head_">Degree Certificate</h3>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="row pb-2">-->
+<!--          <div class="col-lg-4 border border-light rounded">-->
+<!--            <div class="row pt-2">-->
+<!--              <div class="col-1">-->
+<!--                <a href="#">-->
+<!--                  <img class="mt-1" src="./Group.png" alt="Group" height="24" width="auto">-->
+<!--                </a>-->
+<!--              </div>-->
+<!--              <div class="col-6">-->
+<!--                <div>-->
+<!--                  <span>Degree.pdf</span>-->
+<!--                </div>-->
+<!--                <div>-->
+<!--                  <span class="text-black-50">182 KB</span>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--              <div class="col-2"  @click="deleteresume()">-->
+<!--                <a href="#">-->
+<!--                  <img class="float-right mt-1" src="./trash.png" height="20" width="auto">-->
+<!--                </a>-->
+<!--              </div>-->
+<!--              <div class="col-2" @click=" downloadresume()">-->
+<!--                <a href="#">-->
+<!--                  <img class="mr-2 mt-1" src="./download.png" height="20" width="auto">-->
+<!--                </a>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+
+<!--        <hr class="py-2">-->
+
+<!--        <div class="row">-->
+<!--          <div class="col-12">-->
+<!--            <h3 class="text-primary head_">NYSC Discharge Certificate</h3>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="row pb-2">-->
+<!--          <div class="col-lg-4 border border-light rounded">-->
+<!--            <div class="row pt-2">-->
+<!--              <div class="col-1">-->
+<!--                <a href="#">-->
+<!--                  <img class="mt-1" src="./Group.png" alt="Group" height="24" width="auto">-->
+<!--                </a>-->
+<!--              </div>-->
+<!--              <div class="col-6">-->
+<!--                <div>-->
+<!--                  <span>NYSC.pdf</span>-->
+<!--                </div>-->
+<!--                <div>-->
+<!--                  <span class="text-black-50">182 KB</span>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--              <div class="col-2" @click="deleteresume()">-->
+<!--                <a href="#">-->
+<!--                  <img class="float-right mt-1" src="./trash.png" height="20" width="auto">-->
+<!--                </a>-->
+<!--              </div>-->
+<!--              <div class="col-2" @click=" downloadresume(file)">-->
+<!--                <a >-->
+<!--                  <img class="mr-2 mt-1" src="./download.png" height="20" width="auto">-->
+<!--                </a>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
 
         <hr class="py-2">
 
@@ -135,8 +138,9 @@
 </template>
 
 <script>
-import axios from "axios";
 
+import axios from "axios";
+import jspdf from 'jspdf';
 export default {
   name: "resume",
 
@@ -147,7 +151,7 @@ export default {
     };
   },
   beforeCreate() {
-    axios.get(`${process.env.VUE_ROOT_URL}/resume/${this.$route.params.id}`, {
+    axios.get(`${process.env.VUE_ROOT_URL}/certificate/${this.$route.params.id}`, {
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
       }
@@ -165,6 +169,20 @@ export default {
   methods: {
     downloadresume(){
            alert("successfully download");
+           axios({
+             url:"http://9ac930ef93f5.ngrok.io/static/uploads/0fd909cb-7bb8-4e6f-b9d2-166776cd552c-docs-1616925141741.pdf",
+             method:'get',
+             responseType:'blob',
+           })
+      .then((res)=>{
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', '0fd909cb-7bb8-4e6f-b9d2-166776cd552c-docs-1616925141741.pdf');
+        document.body.appendChild(link);
+        console.log("filedata",res.data);
+        link.click();
+      })
     },
     deleteresume(){
       alert("NYSC.pdf");
