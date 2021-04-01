3cfadbfe7f9bdb9f2b21b361a57ccf8cce425ec3
Syed Ahsan Ali <syedahsanali995@gmail.com>
2021-04-01 15:46:56 +0500

    Signup page changes

--- src/components/Signup/Signup.vue
+++ src/components/Signup/Signup.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="height-login-card">
     <div class="login-card pb-30">
-  <a-form :form="form" @submit="handleSubmit">
+      <a-form :form="form" @submit="handleSubmit">
 
-    <a-form-item
-      :label-col="formItemLayout.labelCol"
-      :wrapper-col="formItemLayout.wrapperCol"
-    >
-      <a-input
-        v-decorator="[
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+        >
+          <a-input
+            v-decorator="[
           'firstName',
           { rules: [{ required: true, message: 'Please input your name' }] },
         ]"
-        placeholder="User Name"
-        class="name"
-      />
-    </a-form-item>
+            placeholder="User Name"
+            class="name"
+          />
+        </a-form-item>
 
-    <a-form-item v-bind="formItemLayout">
-      <a-input
-        v-decorator="[
+        <a-form-item v-bind="formItemLayout">
+          <a-input
+            v-decorator="[
           'emailAddress',
           {
             rules: [
@@ -35,13 +35,13 @@
             ],
           },
         ]"
-        placeholder=" Email address *"
-        class="name"
-      />
-    </a-form-item>
-    <a-form-item v-bind="formItemLayout" has-feedback>
-      <a-input
-        v-decorator="[
+            placeholder=" Email address *"
+            class="name"
+          />
+        </a-form-item>
+        <a-form-item v-bind="formItemLayout" has-feedback>
+          <a-input
+            v-decorator="[
           'password',
           {
             rules: [
@@ -55,14 +55,14 @@
             ],
           },
         ]"
-        type="password"
-        placeholder=" Password *"
-        class="name"
-      />
-    </a-form-item>
-    <a-form-item v-bind="formItemLayout"  has-feedback>
-      <a-input
-        v-decorator="[
+            type="password"
+            placeholder=" Password *"
+            class="name"
+          />
+        </a-form-item>
+        <a-form-item v-bind="formItemLayout" has-feedback>
+          <a-input
+            v-decorator="[
           'confirm',
           {
             rules: [
@@ -76,26 +76,26 @@
             ],
           },
         ]"
-        type="password"
-        @blur="handleConfirmBlur"
-        placeholder=" Confirm Password *"
-        class="name"
-      />
-    </a-form-item>
-    <a-form-item  class="agreement">
-      <a-checkbox v-decorator="['agreement', { valuePropName: 'checked' }]">
-        I have read the
-        <a href="">
-          agreement
-        </a>
-      </a-checkbox>
-    </a-form-item>
-    <a-form-item  class="btn">
-      <a-button type="primary" html-type="submit" class="go-back-button-style" @click="openNotification">
-        Register
-      </a-button>
-    </a-form-item>
-  </a-form>
+            type="password"
+            @blur="handleConfirmBlur"
+            placeholder=" Confirm Password *"
+            class="name"
+          />
+        </a-form-item>
+        <a-form-item class="agreement">
+          <a-checkbox v-decorator="['agreement', { valuePropName: 'checked' }]">
+            I have read the
+            <a href="">
+              agreement
+            </a>
+          </a-checkbox>
+        </a-form-item>
+        <a-form-item class="btn">
+          <a-button type="primary" html-type="submit" class="go-back-button-style" @click="openNotification">
+            Register
+          </a-button>
+        </a-form-item>
+      </a-form>
     </div>
   </div>
 </template>
@@ -103,55 +103,18 @@
 <script>
 import axios from "axios";
 
-const residences = [
-  {
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [
-      {
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [
-          {
-            value: 'xihu',
-            label: 'West Lake',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [
-      {
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [
-          {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-          },
-        ],
-      },
-    ],
-  },
-];
 export default {
   data() {
-
     return {
       confirmDirty: false,
-      residences,
-      autoCompleteResult: [],
       formItemLayout: {
         labelCol: {
-          xs: { span: 24 },
-          sm: { span: 8 },
+          xs: {span: 24},
+          sm: {span: 8},
         },
         wrapperCol: {
-          xs: { span: 24 },
-          sm: { span: 16 },
+          xs: {span: 24},
+          sm: {span: 16},
         },
       },
       tailFormItemLayout: {
@@ -169,33 +132,30 @@ export default {
     };
   },
   beforeCreate() {
-    this.form = this.$form.createForm(this, { name: 'register' });
+    this.form = this.$form.createForm(this, {name: 'register'});
   },
   methods: {
     handleSubmit(e) {
-      console.log('consoledata',process.env)
       e.preventDefault();
       this.form.validateFieldsAndScroll((err, values) => {
         if (!err) {
-          console.log('Received values of form: ', values);
-          const that=this;
+          const that = this;
           this.$router.push({path: '/job-seeker/login'});
           var data = values
           var config = {
             method: 'post',
             url: `${process.env.VUE_ROOT_URL}/user/signup`,
             headers: {
-              'Authorization':  `Bearer ${localStorage.getItem('token')}`,
+              'Authorization': `Bearer ${localStorage.getItem('token')}`,
               'Content-Type': 'application/json'
             },
-            data : data
+            data: data
           };
           axios(config)
             .then(function (response) {
-              console.log(JSON.stringify(response.data));
-              localStorage.setItem('user_id',response.data.userData.id);
-              that.$router.push({path: '/job-seeker/login'});
-            }
+                localStorage.setItem('user_id', response.data.userData.id);
+                that.$router.push({path: '/job-seeker/login'});
+              }
             )
             .catch(function (error) {
               console.log(error);
@@ -207,6 +167,7 @@ export default {
       const value = e.target.value;
       this.confirmDirty = this.confirmDirty || !!value;
     },
+
     compareToFirstPassword(rule, value, callback) {
       const form = this.form;
       if (value && value !== form.getFieldValue('password')) {
@@ -215,36 +176,20 @@ export default {
         callback();
       }
     },
+
     validateToNextPassword(rule, value, callback) {
       const form = this.form;
       if (value && this.confirmDirty) {
-        form.validateFields(['confirm'], { force: true });
+        form.validateFields(['confirm'], {force: true});
       }
       callback();
     },
 
-    gotologin: function (e) {
-      if (this.form) { return true; }
-
-      this.errors = [];
-
-      if (!this.email) {
-        this.errors.push({path: '/job-seeker/login'});
-      }
-      e.preventDefault();
-    },
-
-    // gotologin: function (step) {
-    //   this.$router.push({path: '/job-seeker/login'});
-    // },
     openNotification() {
       this.$notification.open({
         message: 'Success',
         description:
-          'You have SignUp Successfully.',
-        onClick: () => {
-          console.log('Notification Clicked!');
-        },
+          'You have SignUp Successfully.'
       });
     },
   },
@@ -257,6 +202,7 @@ export default {
 .height-login-card {
   height: calc(100vh + 10px);
 }
+
 .login-card {
   position: absolute;
   padding: 32px;
@@ -268,6 +214,7 @@ export default {
   box-shadow: 0px 4px 20px rgba(115, 115, 115, 0.2);
   border-radius: 10px;
 }
+
 .go-back-button-style {
   background: #fafafa;
   border-radius: 4px;
@@ -280,13 +227,16 @@ export default {
   height: 48px;
   border: 1px solid #fafafa;
 }
-.btn{
+
+.btn {
   margin-top: 10px;
 }
-.agreement{
+
+.agreement {
   margin-top: 10px;
 }
-.name{
+
+.name {
   width: 150%;
 }
 </style>
