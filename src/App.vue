d967d3018cbd90acc85dc4fc8d490ba9f16e2044
cosmos <muhammadhurairaaamir@gmail.com>
2021-05-21 11:30:29 +0500

    d

--- src/App.vue
+++ src/App.vue
@@ -18,38 +18,33 @@ export default {
 <style>
 /*@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400&display=swap');*/
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400&display=swap');
-
-* {
+*{
   font-family: "Roboto", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 
 }
-
-@media screen and (min-width: 769px) {
-  #app {
+@media screen and (min-width:769px){
+  #app{
     padding-top: 80px;
   }
 }
 
-.ant-form-item-control-wrapper {
-  display: inline-block;
-  vertical-align: top;
-  width: 100% !important;
+.ant-form-item-control-wrapper{
+    display: inline-block;
+    vertical-align: top;
+    width: 100%!important;
 }
-
-.dropdwon-fonts > div {
+.dropdwon-fonts>div{
   height: 48px;
   display: flex;
   align-items: center;
 }
-
-.searchbox-style input {
+.searchbox-style input{
   height: 48px;
   font-family: 'Roboto', sans-serif;
 
 }
-
 .searchbox-style input::-webkit-input-placeholder { /* Edge */
   font-style: normal;
   font-weight: 500;
@@ -73,38 +68,19 @@ export default {
   line-height: 160%;
   color: #8B90A0;
 }
-
-.date-picker input {
+.date-picker input{
   height: 48px
 }
-
-.dropdown-select > div {
+.dropdown-select >div{
   height: 48px !important
 }
-
-.dropdown-select li {
+.dropdown-select li{
   height: 38px !important;
   display: flex;
   align-items: center;
 }
-
-.btn-primary {
-  background: #0385F3 !important;
-}
-
-.btn {
-  font-family: 'Open sans', sans-serif;
-}
-
-.ant-modal-content {
-  border-radius: 8px;
-}
-
-.button-class {
-  column-gap: 10px;
-}
 .ant-tabs-bar ,.ant-tabs-bar ,.ant-tabs-top-bar ,.ant-tabs-small-bar{
-  width:245px!important;
+  width: 277px!important;
   margin-left: 40px;
 }
 .ant-table-thead tr{
@@ -140,7 +116,6 @@ export default {
 .button-class{
   column-gap: 10px;
 }
-
 .login-button-style,
 .go-back-button-style {
   background: #fafafa;
@@ -156,11 +131,9 @@ export default {
   height: 48px;
   border: 1px solid #fafafa;
 }
-
 .login-button-style {
   background: #0385f3;
   color: #fff;
 }
-
 @import "~bootstrap/dist/css/bootstrap.css";
 </style>
